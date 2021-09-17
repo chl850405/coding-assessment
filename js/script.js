6e4adbebb49c71a260fa1c2e60568394115c8618
christi marchetti <christimarchetti@gmail.com>
2021-09-16 22:57:25 -0400

    outline

---
+++ js/script.js
@@ -0,0 +1,25 @@
+//Guidelines for the quiz
+//You will get 3 minutes to complete the quiz
+//Once an answer is selected the next question will be presented
+//If a question is answered incorrectly time is subtracted from the clock
+//Once all answers are completed or the time reaches 0 the quiz is over
+
+//Which declaration defines a document?
+//<!DOCTYPE html>
+//<header>
+//<h1>
+//<!doctype>
+
+//What does HTML stand for?
+//Hyper Text Markup List
+//Hyper Text Markup Language
+//Hyper Touch Markup Language
+//Hyper Text Mandate Language
+
+//How do you add a folder using the terminal?
+//touch
+//git add .
+//mkdir
+//git commit -m
+
+
